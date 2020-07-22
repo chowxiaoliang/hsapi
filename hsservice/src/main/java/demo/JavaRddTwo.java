6c17ba225618a0c40181c1fd39610630168e7e43
zhouliang <liang.zhou@skyroam.com>
2020-07-22 17:05:24 +0800

    javaRdd

--- hsservice/src/main/java/demo/JavaRddTwo.java
+++ hsservice/src/main/java/demo/JavaRddTwo.java
@@ -3,18 +3,24 @@ package demo;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.codehaus.janino.Java;
+import scala.Tuple2;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class JavaRddTwo {
     public static void main(String[] args) {
-
+        System.out.println("==========================");
         SparkConf sparkConf = new SparkConf();
         sparkConf.setMaster("local[2]").setAppName("JavaRddTwo");
         JavaSparkContext javaSparkContext = new JavaSparkContext(sparkConf);
 
-        JavaRDD<String> javaRDD = javaSparkContext.parallelize(Arrays.asList("1","2","3","4"));
+        JavaRDD<String> javaRdd = javaSparkContext.parallelize(Arrays.asList("1","2","3","4","5","5"));
+        JavaRDD<String> rdd = javaRdd.distinct();
+
+        List<String> result = rdd.collect();
+        result.forEach(System.out::println);
 
     }
 }
