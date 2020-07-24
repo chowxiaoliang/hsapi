799bf5acab398cc44fca8ad28a98260cdd90d291
zhouliang <liang.zhou@skyroam.com>
2020-07-24 18:21:26 +0800

    javaRdd

--- hsservice/src/main/java/demo/JavaRddTwo.java
+++ hsservice/src/main/java/demo/JavaRddTwo.java
@@ -11,6 +11,7 @@ import org.codehaus.janino.Java;
 import scala.Tuple2;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class JavaRddTwo {
@@ -44,10 +45,30 @@ public class JavaRddTwo {
         // 取两个rdd的交集
 //        JavaRDD<String> rdd = javaRdd.intersection(anotherRDD);
 
-        // 对rdd重新分区，返回一个新的rdd
-        JavaRDD<String> rdd = javaRdd.coalesce(2);
-        JavaRDD<String> rdd = javaRdd.repartition(2);
-        List<String> result = rdd.collect();
+        // 对rdd重新分区，但是不进行shuffle
+//        JavaRDD<String> rdd = javaRdd.coalesce(2);
+
+        // 对rdd重新分区，true表示进行shuffle
+//        JavaRDD<String> rdd = javaRdd.coalesce(2, true);
+
+        // 底层默认调用的是coalesce(2, true)，上一个例子
+//        JavaRDD<String> rdd = javaRdd.repartition(2);
+
+        // flatMap接收一个变量，返回一个iterator
+//        JavaRDD<String> rdd = javaRdd.flatMap(x -> Collections.singletonList(x).iterator());
+
+        // map接收一个变量，返回一个变量
+//        JavaRDD<Tuple2<Integer, Integer>> rdd = javaRdd.map(x -> new Tuple2<>(Integer.parseInt(x), Integer.parseInt(x)));
+//        List<Tuple2<Integer, Integer>> result = rdd.collect();
+
+        // mapToPair接收一个变量，返回一个二元组
+//        JavaPairRDD<Integer, Integer> rdd = javaRdd.mapToPair(x -> new Tuple2<>(Integer.parseInt(x), Integer.parseInt(x)));
+//        List<Tuple2<Integer, Integer>> result = rdd.collect();
+
+        // 根据序号生成键值对rdd
+        JavaPairRDD<String, Long> rdd = javaRdd.zipWithIndex();
+        List<Tuple2<String, Long>> result = rdd.collect();
+//        List<String> result = rdd.collect();
         result.forEach(System.out::println);
 
     }
