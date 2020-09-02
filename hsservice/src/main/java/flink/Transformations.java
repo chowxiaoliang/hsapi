c1895d937a7f10dc57c390cced813eeecf431241
zhouliang <liang.zhou@skyroam.com>
2020-09-02 11:02:57 +0800

    更新参考内容

--- hsservice/src/main/java/flink/Transformations.java
+++ hsservice/src/main/java/flink/Transformations.java
@@ -14,6 +14,7 @@ import java.util.stream.StreamSupport;
 
 /**
  * flink 相关的算子 参考:https://www.jianshu.com/p/a0bb414b4c81
+ *                      https://www.jianshu.com/p/6a726f2f7137
  */
 public class Transformations {
 
