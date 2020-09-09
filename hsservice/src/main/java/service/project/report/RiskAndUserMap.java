bee60b9325361dff76613b3c6c28ba86cf2846cb
zhouliang <liang.zhou@skyroam.com>
2020-09-09 18:35:12 +0800

    flink实现数据写到redis

--- hsservice/src/main/java/service/project/report/RiskAndUserMap.java
+++ hsservice/src/main/java/service/project/report/RiskAndUserMap.java
@@ -13,10 +13,17 @@ import org.apache.flink.streaming.api.functions.sink.PrintSinkFunction;
 import org.apache.flink.streaming.api.functions.sink.SinkFunction;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.streaming.connectors.redis.RedisSink;
+import org.apache.flink.streaming.connectors.redis.common.config.FlinkJedisClusterConfig;
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisCommandDescription;
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisMapper;
 import org.apache.flink.util.Collector;
 import spark.streaming.KafkaDataSource;
 
+import java.net.InetSocketAddress;
+import java.util.HashSet;
 import java.util.Properties;
+import java.util.Set;
 
 public class RiskAndUserMap {
 
@@ -41,19 +48,45 @@ public class RiskAndUserMap {
         properties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 
         DataStreamSource<String> dataStreamSource = streamExecutionEnvironment.addSource(new FlinkKafkaConsumer<>(topic, new SimpleStringSchema(), properties));
-        dataStreamSource.setParallelism(4);
+
+        InetSocketAddress inetSocketAddress = new InetSocketAddress("192.168.229.128", 6379);
+        Set<InetSocketAddress> set = new HashSet<>();
+        set.add(inetSocketAddress);
+        FlinkJedisClusterConfig flinkJedisClusterConfig = new FlinkJedisClusterConfig.Builder().setNodes(set).build();
+
+
         DataStream<Tuple2<String, Integer>> dataStream = dataStreamSource.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
             @Override
             public void flatMap(String s, Collector<Tuple2<String, Integer>> collector) throws Exception {
                 for(String letter : s.split(" ")){
+
                     collector.collect(new Tuple2<>(letter, 1));
                 }
             }
         }).keyBy(0).timeWindow(Time.seconds(1), Time.seconds(1)).sum(1);
 //        dataStream.print();
-        // 同样的效果
-        dataStream.addSink(new PrintSinkFunction<>());
+        // 同样的效果(打印数据)
+//        dataStream.addSink(new PrintSinkFunction<>());
+        dataStream.addSink(new RedisSink<scala.Tuple2<String, Integer>>(flinkJedisClusterConfig, new myRedisMapper()))
         streamExecutionEnvironment.execute("test");
 
+    }
+
+    private static class myRedisMapper implements RedisMapper<scala.Tuple2<String, Integer>>{
+
+        @Override
+        public RedisCommandDescription getCommandDescription() {
+            return null;
+        }
+
+        @Override
+        public String getKeyFromData(scala.Tuple2<String, Integer> stringIntegerTuple2) {
+            return null;
+        }
+
+        @Override
+        public String getValueFromData(scala.Tuple2<String, Integer> stringIntegerTuple2) {
+            return null;
+        }
     }
 }
